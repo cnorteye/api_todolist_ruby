6dcdd636cae061923c0073843a0d12232c011bac
cnorteye <cnortye@gmail.com>
2020-11-14 20:58:07 -0700

    changes in item_serializer.rb

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,9 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name
+
+  def items
+    self.object.items.map do |item|
+      {id: item.id, name:item.name, done:item.done}		
+    end		
+  end
 end
