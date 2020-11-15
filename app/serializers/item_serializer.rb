9ab00b14a6cff2a7afcdb5ba562fea749b270e81
cnorteye <cnortye@gmail.com>
2020-11-14 21:31:09 -0700

    changes in the item_serializer.rb

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,9 +1,8 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name
-
-  def items
-    self.object.items.map do |item|
-      {id: item.id, name:item.name, done:item.done}		
-    end		
-  end
+  attributes :id, name
+  # def items
+  #   self.object.items.map do |item|
+  #     {id: item.id, name:item.name, done:item.done}		
+  #   end		
+  # end
 end
